42c15d972a8edc4987d6cfe8aad161917770b37e
edsoncarlosdvp <edsoncarlos@ymail.com>
2021-11-11 11:11:55 -0300

    Static Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 import MyComponent from '../myComponent/myComponent'
 
 // Step 2: Define your component
@@ -12,6 +13,10 @@ const IndexPage = () => {
         <Link to="/about">Sobre</Link>
         <p>I'm making this by following the Gatsby Tutorial.</p>
         <MyComponent />
+        <StaticImage
+          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+          src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        />
       </Layout>
     </main>
   )
